872a40996535e7b33231e096bce5a357988a1ee9
John MacFarlane <jgm@berkeley.edu>
2011-02-01 22:50:23 -0800

    Small fix to allow compilation with pandoc 1.8.

--- Yst/Render.hs
+++ Yst/Render.hs
@@ -22,7 +22,7 @@ import Yst.Types
 import Yst.Util
 import Yst.Data
 import System.Directory
-import Text.Pandoc
+import Text.Pandoc hiding (Format)
 import Text.XHtml hiding (option, (</>))
 import Data.Char
 import Data.List (intercalate)
