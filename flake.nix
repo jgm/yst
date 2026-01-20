83c08002531a60d212ebe941d5be24eab78a9803
John MacFarlane <jgm@berkeley.edu>
2026-01-20 12:33:48 +0100

    Add flake.

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "pandoc";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        haskellPackages = pkgs.haskellPackages;
+        jailbreakUnbreak = pkg:
+          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+
+        # DON'T FORGET TO PUT YOUR PACKAGE NAME HERE, REMOVING `throw`
+        packageName = "pandoc";
+      in {
+        packages.${packageName} =
+          haskellPackages.callCabal2nix packageName self rec {
+            # Dependency overrides go here
+          };
+
+        packages.default = self.packages.${system}.${packageName};
+        defaultPackage = self.packages.${system}.default;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            haskellPackages.haskell-language-server # you must build it with your ghc to work
+            haskellPackages.hlint
+            haskellPackages.cabal-install
+            haskellPackages.cabal-plan
+            haskellPackages.weeder
+            haskellPackages.hpc
+            haskellPackages.ghcid
+            haskellPackages.stylish-haskell
+            haskellPackages.eventlog2html
+            haskellPackages.profiterole
+            haskellPackages.profiteur
+            zlib.dev
+            git
+            bashInteractive
+            epubcheck # for validate-epub
+            nodejs # for validate-epub
+            ripgrep
+            libxml2 # for xmllint
+            jq
+            sqlite.dev
+          ];
+          inputsFrom = map (__getAttr "env") (__attrValues self.packages.${system});
+        };
+        devShell = self.devShells.${system}.default;
+      });
+}
